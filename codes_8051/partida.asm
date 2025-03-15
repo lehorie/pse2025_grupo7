47dac27a4822a219a92d5bc7eb2f800939188fc2
DanielEccard <83174780+DanielEccard@users.noreply.github.com>
2025-03-15 11:52:27 -0300

    Update partida.asm

--- codes_8051/partida.asm
+++ codes_8051/partida.asm
@@ -5,7 +5,7 @@ org    0003h
 jmp    isrext0	;Desligar o motor
 
 org    0013h	
-jmp    isrext1	;Revers„o do sentido de rotaÁ„o
+jmp    isrext1	;Revers√£o do sentido de rota√ß√£o
 
 isrext0:
 		setb P1.0
@@ -20,34 +20,33 @@ isrext1:
 		reti
 
 partida_inicial:
-		jb		P2.3, partida_inicial	;Aguarda  o operador acionar o bot„o para ligar o motor
-        jnb		P2.3, $
-		mov		ie, #10000101b
-        mov     tcon, #00000101b
-		cpl P1.0
-		cpl P1.2
-		setb P2.4
-		setb P2.5
-		
-		;cÛdico do start
+		jb	P2.3, partida_inicial	;Aguarda  o operador acionar o bot√£o para ligar o motor
+        	jnb	P2.3, $
+		mov	ie, #10000101b
+       		mov	tcon, #00000101b
+		cpl 	P1.0
+		cpl 	P1.2
+		setb 	P2.4
+		setb 	P2.5
+
 loop:
-		jnb P2.5, desliga_motor
-		jmp check_inversao
+		jnb 	P2.5, desliga_motor
+		jmp 	check_inversao
 
 check_inversao:
-		jnb P2.4, inverte_sentido
-		jmp loop
+		jnb 	P2.4, inverte_sentido
+		jmp 	loop
 
 desliga_motor:
-		jnb P2.5, desliga_motor
+		jnb 	P2.5, desliga_motor
 		clr	P3.2
-		setb P3.2
-		jmp partida_inicial
+		setb 	P3.2
+		jmp 	partida_inicial
 
 inverte_sentido:
-		jnb P2.4, inverte_sentido ;Aguarda o pulso de subida
-		clr P3.3 ;ativa interrupÁ„o 1        
-		setb P3.3
-		jmp loop
+		jnb 	P2.4, inverte_sentido ;Aguarda o pulso de subida
+		clr 	P3.3 ;ativa interrup√ß√£o 1        
+		setb 	P3.3
+		jmp 	loop
 
-        end
+        	end
